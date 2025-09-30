139bc1b4159df0b775232bb6b8edac366087b5d6
Padam Gadshila <padamgadshila17@gmail.com>
2025-09-30 12:08:01 +0530

    Add TodoInput component for adding new todos and integrate it into Index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import { createHomeStyles } from "@/assets/styles/home.styles";
 import Header from "@/components/Header";
+import TodoInput from "@/components/TodoInput";
 import useTheme from "@/hooks/useTheme";
 import { LinearGradient } from "expo-linear-gradient";
 import { StatusBar, Text, TouchableOpacity } from "react-native";
@@ -17,6 +18,7 @@ export default function Index() {
 
       <SafeAreaView style={styles.safeArea}>
         <Header />
+        <TodoInput />
         <TouchableOpacity onPress={toggleDarkMode}>
           <Text>Change the theme</Text>
         </TouchableOpacity>
